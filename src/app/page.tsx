7bd6e60ca399038703fdc7c716248b25f4f59635
Ivan Egorov <ivan.egorov@cdek.ru>
2023-12-24 00:24:08 +0700

    chore: update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home(): JSX.Element | null {
-  return <h1 className='p-16 text-center'>E-Reception</h1>;
+  return (
+    <h2 className='scroll-m-20 border-b pt-8 text-center text-3xl font-semibold tracking-tight first:mt-0'>
+      E-Reception
+    </h2>
+  );
 }
