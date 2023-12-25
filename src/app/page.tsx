cb692396339a23502468c7db9a60865135c21983
Ivan Egorov <ezone225@gmail.com>
2023-12-26 03:08:15 +0700

    feat(share): Configure shadcn-ui components (#11)
    closes #5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+
 export default function Home(): JSX.Element | null {
+  const handleClick = (): void => {
+    console.log('You clicked me!');
+  };
   return (
-    <h2 className='scroll-m-20 border-b pt-8 text-center text-3xl font-semibold tracking-tight first:mt-0'>
-      E-Reception
-    </h2>
+    <>
+      <h2 className='scroll-m-20 border-b pt-8 text-center text-3xl font-semibold tracking-tight first:mt-0'>
+        E-Reception
+      </h2>
+      <div>
+        <Button className='ml-16 mt-8' onClick={handleClick}>
+          Click me
+        </Button>
+      </div>
+    </>
   );
 }
